09fed6ed54048f6b1f69ee728765c2df89afd9dd
eylee02 <leywindy@naver.com>
2023-09-19 17:42:57 +0900

    20230919

--- 07_dbcp/src/main/java/ctrl/BoardController.java
+++ 07_dbcp/src/main/java/ctrl/BoardController.java
@@ -8,20 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.ActionForward;
-import service.MvcService;
-import service.MvcServiceImpl;
+import service.BoardService;
+import service.BoardServiceImpl;
 
 /**
- * Servlet implementation class MvcController
+ * Servlet implementation class BoardController
  */
 @WebServlet("*.do")
-public class MvcController extends HttpServlet {
+public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MvcController() {
+    public BoardController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,44 +29,46 @@ public class MvcController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-    	// 요청 인코딩 + 응답 타입과 인코딩
-		request.setCharacterEncoding("UTF-8");
+		// BoardFilter 실행 후 Controller 실행
+		
+		// 요청 인코딩(BoardFilter가 수행함) + 응답 타입과 인코딩
+		// request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html charset=UTF-8");
 		
-		// 요청 확인
+		// 요청 주소 확인
 		String requestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String urlMapping = requestURI.substring(contextPath.length());
 		
-		// 서비스 객체 생성
-		MvcService mvcService = new MvcServiceImpl();
-		
-		// ActionForward 객체 생성 (서비스의 실행 결과를 저장할 객체)
+		// 어디로 어떻게 이동할 것인지 알고 있는 ActionForward 객체
 		ActionForward af = null;
 		
-		// 요청에 따른 서비스 선택과 실행
+		// BoardService 객체 생성
+		BoardService boardService = new BoardServiceImpl();
+		
+		// 요청에 따른 처리
 		switch(urlMapping) {
-		case "/getAge.do":
-		  af = mvcService.getAge(request);
-		  break;
-		case "/getAbs.do": 
-		  mvcService.getAbs(request, response);
-		  break;
-		
-		case "/getInfo.do": 
-		  af = mvcService.getInfo(request);
-		  break;
-		
+		// 단순 이동 (forward 처리)
+		case "/board/list.do":
+			af = new ActionForward("/board/list.jsp", false);
+			break;
+		case "/board/write.do":
+			af = new ActionForward("/board/write.jsp", false);
+			break;
+			// 서비스 처리
+		case "/board/register.do":
+			af = boardService.register(request);
+			break;
 		}
 		
 		// 이동
 		if(af != null) {
 			if(af.isRedirect()) {
-			  response.sendRedirect(af.getPath());
+				response.sendRedirect(af.getPath());
 			} else {
-			  request.getRequestDispatcher(af.getPath()).forward(request, response);
+				request.getRequestDispatcher(af.getPath()).forward(request, response);
 			}
 		}
 		
